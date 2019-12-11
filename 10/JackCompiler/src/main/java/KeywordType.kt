912642e332829a4599cb2952f697226484d9daf8
962n <tak.k1u2r2o4@gmail.com>
2019-12-11 20:01:53 +0900

    add keywordtype

--- 10/JackCompiler/src/main/java/KeywordType.kt
+++ 10/JackCompiler/src/main/java/KeywordType.kt
@@ -1,3 +1,29 @@
-enum class KeywordType {
-    CLASS
+enum class KeywordType constructor(val value: String) {
+    CLASS("class"),
+    CONSTRUCTOR("constructor"),
+    FUNCTION("function"),
+    METHOD("method"),
+    FIELD("field"),
+    STATIC("static"),
+    VAR("var"),
+    INT("int"),
+    CHAR("char"),
+    BOOLEAN("boolean"),
+    VOID("void"),
+    TRUE("true"),
+    FALSE("false"),
+    NULL("null"),
+    THIS("this"),
+    LET("let"),
+    DO("do"),
+    IF("if"),
+    ELSE("else"),
+    WHILE("while"),
+    RETURN("return");
+
+    companion object {
+        fun of(value: String): KeywordType? {
+            return values().firstOrNull { value == it.value }
+        }
+    }
 }
--- 10/JackCompiler/src/main/java/Pattern.kt
+++ 10/JackCompiler/src/main/java/Pattern.kt
@@ -1,35 +1,32 @@
 object Pattern {
 
     object Token {
-        val keyword:String get() {
-            val list = listOf(
-                    "class","constructor","function","method",
-                    "field","static","var","int","char","boolean",
-                    "void","true","false","null","this","let",
-                    "do","if","else","while","return"
-            )
-            return list.fold(""){ init , element ->
-                return "$init$element|"
-            }.apply {
-                val removeSuffix = this.removeSuffix("|")
-                """($removeSuffix)"""
+        val keyword: String
+            get() {
+                val list = KeywordType.values().map { it.value }
+                return list.fold("") { init, element ->
+                    return "$init$element|"
+                }.let {
+                    val removeSuffix = it.removeSuffix("|")
+                    """($removeSuffix)"""
+                }
             }
-        }
-        val symbol:String get() {
-            val list = listOf(
-                    "{","}","(",")", "[","]",
-                    ".",",",";",
-                    "+", "-","*","/",
-                    "&","|",
-                    "<", ">","=","~"
-            )
-            return list.fold(""){ init , element ->
-                return "$init$element|"
-            }.apply {
-                val removeSuffix = this.removeSuffix("|")
-                """($removeSuffix)"""
+        val symbol: String
+            get() {
+                val list = listOf(
+                        "{", "}", "(", ")", "[", "]",
+                        ".", ",", ";",
+                        "+", "-", "*", "/",
+                        "&", "|",
+                        "<", ">", "=", "~"
+                )
+                return list.fold("") { init, element ->
+                    return "$init$element|"
+                }.let {
+                    val removeSuffix = it.removeSuffix("|")
+                    """($removeSuffix)"""
+                }
             }
-        }
         const val integerConstant = """([0-9]|[1-9]+[0-9])"""
         const val stringConstant = """"""
         const val identifier = """([a-zA-Z_][a-zA-Z0-9_]*)"""
